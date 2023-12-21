cba43936856d74232ae8c9283646cad3ec2774bc
virajpate001 <virajpate001@gmail.com>
2023-12-21 19:21:10 +0530

    mobile menu functionality added

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $(".navicon").click(function(){
+      $(".bx-menu").toggleClass("bx-x");
+    });
+
+    $(".navicon").click(function(){
+      $(".mob-navbar").toggleClass("dv");
+    });
+
+});
