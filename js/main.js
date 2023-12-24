9e66436bb914e9d8a641e371d069ea1292d9fe22
virajpate001 <virajpate001@gmail.com>
2023-12-24 14:07:42 +0530

    Portfolio page complete

--- js/main.js
+++ js/main.js
@@ -7,4 +7,27 @@ $(document).ready(function(){
       $(".mob-navbar").toggleClass("dv");
     });
 
+    
+    
+
 });
+
+// portfolio filter
+function filterCategory(element,category) {
+  const items = document.querySelectorAll('.portfolio-item');
+  const catbutons = document.querySelectorAll('.category');
+
+  catbutons.forEach(btn => btn.classList.remove('activecat'));
+  element.classList.add('activecat');
+
+  items.forEach(item => {
+      if (category === 'all' || item.classList.contains(category)) {
+          item.style.display = "block";
+      } else {
+          item.style.display = "none";
+      }
+  });
+}
+
+// portfolio add active class
+
